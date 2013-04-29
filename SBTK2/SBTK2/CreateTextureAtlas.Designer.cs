6d87363d36696a6bb88d74a300e815c2e419dc99
Johan Fredriksson <johanfredriksson1987@gmail.com>
2013-04-29 22:02:16 +0200

    Created GUI.
    Signed-off-by: Johan Fredriksson <johanfredriksson1987@gmail.com>

---
+++ SBTK2/SBTK2/CreateTextureAtlas.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace SBTK2
+{
+    partial class FormTextureAtlas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panelTextureCollector = new System.Windows.Forms.Panel();
+            this.panelCutTexture = new System.Windows.Forms.Panel();
+            this.btnAddImage = new System.Windows.Forms.Button();
+            this.listViewAddedTextures = new System.Windows.Forms.ListView();
+            this.loadedImages = new System.Windows.Forms.ImageList(this.components);
+            this.SuspendLayout();
+            // 
+            // panelTextureCollector
+            // 
+            this.panelTextureCollector.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.panelTextureCollector.BackColor = System.Drawing.Color.Teal;
+            this.panelTextureCollector.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panelTextureCollector.Location = new System.Drawing.Point(542, 70);
+            this.panelTextureCollector.Name = "panelTextureCollector";
+            this.panelTextureCollector.Size = new System.Drawing.Size(413, 391);
+            this.panelTextureCollector.TabIndex = 0;
+            // 
+            // panelCutTexture
+            // 
+            this.panelCutTexture.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.panelCutTexture.BackColor = System.Drawing.Color.Red;
+            this.panelCutTexture.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panelCutTexture.ForeColor = System.Drawing.Color.Black;
+            this.panelCutTexture.Location = new System.Drawing.Point(237, 70);
+            this.panelCutTexture.Name = "panelCutTexture";
+            this.panelCutTexture.Size = new System.Drawing.Size(299, 391);
+            this.panelCutTexture.TabIndex = 1;
+            // 
+            // btnAddImage
+            // 
+            this.btnAddImage.Location = new System.Drawing.Point(21, 12);
+            this.btnAddImage.Name = "btnAddImage";
+            this.btnAddImage.Size = new System.Drawing.Size(105, 43);
+            this.btnAddImage.TabIndex = 2;
+            this.btnAddImage.Text = "Add Texture";
+            this.btnAddImage.UseVisualStyleBackColor = true;
+            this.btnAddImage.Click += new System.EventHandler(this.btnAddImage_Click);
+            // 
+            // listViewAddedTextures
+            // 
+            this.listViewAddedTextures.Location = new System.Drawing.Point(21, 70);
+            this.listViewAddedTextures.Name = "listViewAddedTextures";
+            this.listViewAddedTextures.Size = new System.Drawing.Size(187, 391);
+            this.listViewAddedTextures.TabIndex = 0;
+            this.listViewAddedTextures.UseCompatibleStateImageBehavior = false;
+            // 
+            // loadedImages
+            // 
+            this.loadedImages.ColorDepth = System.Windows.Forms.ColorDepth.Depth32Bit;
+            this.loadedImages.ImageSize = new System.Drawing.Size(32, 32);
+            this.loadedImages.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // FormTextureAtlas
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(993, 556);
+            this.Controls.Add(this.btnAddImage);
+            this.Controls.Add(this.listViewAddedTextures);
+            this.Controls.Add(this.panelCutTexture);
+            this.Controls.Add(this.panelTextureCollector);
+            this.Name = "FormTextureAtlas";
+            this.Text = "CreateTextureAtlas";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelTextureCollector;
+        private System.Windows.Forms.Panel panelCutTexture;
+        private System.Windows.Forms.Button btnAddImage;
+        private System.Windows.Forms.ListView listViewAddedTextures;
+        private System.Windows.Forms.ImageList loadedImages;
+    }
+}
+
---
+++ SBTK2/SBTK2/TextureListManager.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace SBTK2
+{
+    class TextureListManager
+    {
+        // uses to validate format.
+        private readonly string[] ALLOWED_IMAGE_EXTENSIONS = { ".jpg", ".jpeg", ".png", ".bmp" };
+
+        List<Image> image = new List<Image>();
+
+        /// <summary>
+        /// Gets or sets the images stored for the thumbnail list.
+        /// </summary>
+        public List<Image> Images
+        {
+            get { return this.image; }
+            set { this.image = value; }
+        }
+
+        /// <summary>
+        /// Gets the number of stored images.
+        /// </summary>
+        public int NrOfImages
+        {
+            get { return image.Count; }
+        }
+
+        /// <summary>
+        /// Gets the image at the specified index
+        /// </summary>
+        /// <param name="index">specified index</param>
+        /// <returns>The image at the index</returns>
+        public Image getImageAtIndex(int index)
+        {
+            return image[index];
+        }
+        string imageExtension = "";
+
+        /// <summary>
+        /// Adds an image object to the list<Image>image
+        /// </summary>
+        /// <param name="imageInput"></param>
+        public void AddImage(Image imageInput)
+        {
+            this.image.Add(imageInput);
+        }
+
+        // Open a image using OpenFileDialog.
+        public void OpenImage()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            //Enables multiselect
+            openFileDialog.Multiselect = true;
+            openFileDialog.Filter = "Image Files(*.jpg; *.jpeg; *.bmp; *.png;)";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                for (int i = 0; i < openFileDialog.FileName.Length; i++)
+                {
+                    this.image.Add(Image.FromFile(openFileDialog.FileName[i]));
+                }
+            }
+               
+        }
+
+        /// <summary>
+        ///  Check to se if the choosen object exist and uses the method IsFileCorrectType to verify that the format is ok.
+        ///  If ok add image to listView via  AddFile and AddImage.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void FileFormats(object sender, DragEventArgs e)
+        {
+            string[] handles = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+            if (handles != null)
+            {
+                foreach (string s in handles)
+                {
+
+                    if (File.Exists(s))
+                    {
+
+                        if (IsFileCorrectType(s, ALLOWED_IMAGE_EXTENSIONS))
+                        {
+                            AddFile(s);
+                        }
+                        else
+                        {
+                            MessageBox.Show("The system only support the formats .jpg .jpeg .png .bmp");
+                        }
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("File not found");
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        ///  Determines if the file has one of the accepted extensions.
+        /// </summary>
+        /// <param name="filePath"></param>
+        /// <param name="validExtensions"></param>
+        /// <returns></returns>
+        private bool IsFileCorrectType(string filePath, string[] validExtensions)
+        {
+            bool isCorrect = false;
+            foreach (string extension in validExtensions)
+            {
+                if (string.Compare(Path.GetExtension(filePath), extension, true) == 0)
+                {
+                    isCorrect = true;
+                    break;
+                }
+            }
+
+            return isCorrect;
+        } 
+    }
+}
